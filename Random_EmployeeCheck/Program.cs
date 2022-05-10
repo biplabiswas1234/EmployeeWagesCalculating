7d4aba5c8448af56f6af2c53cdeff4dc2efc4fa1
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-05-10 16:26:10 +0530

    [Biplab] Add : Part Time or Full Time Employee wages Calculation

--- Random_EmployeeCheck/Program.cs
+++ Random_EmployeeCheck/Program.cs
@@ -1,37 +1,45 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace EmployeePresentAbsent
+namespace PracticeProblem1
 {
     public class Program
     {
         public int EmpPresent = 1;
+        public int FullTimeEmp = 1;
         public int FullDayHour = 8;
+        public int PartTimeHour = 4;
         public int WagesPerHour = 20;
         public void CheckEmpPresentAbsent()
         {
-            Random check = new Random();
-            int CheckEmp = check.Next(0, 2);
-
+            Random Check = new Random();
+            int CheckEmp = Check.Next(0, 2);
             if (EmpPresent == CheckEmp)
             {
-                Console.WriteLine("Employee is Present");
-                int DailyWages = FullDayHour * WagesPerHour;
-                Console.WriteLine("DailyWages: " + DailyWages);
+                Console.WriteLine("Employee is present");
+                Random TimeCheck = new Random();
+                int CheckTimeEmp = TimeCheck.Next(0, 2);
+                if (FullTimeEmp == CheckTimeEmp)
+                {
+                    Console.WriteLine("Employee is FullTimeEmp");
+                    int DailyWages = FullDayHour * WagesPerHour;
+                    Console.WriteLine("DailyWages: " + DailyWages);
+                }
+                else
+                {
+                    Console.WriteLine("Employee is PartTimeEmp");
+                    int DailyWages1 = PartTimeHour * WagesPerHour;
+                    Console.WriteLine("DailyWages: " + DailyWages1);
+                }
             }
             else
             {
                 Console.WriteLine("Employee is Absent");
-                Console.WriteLine("Employee has no wages");
             }
         }
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            Program Obj = new Program();
-            Obj.CheckEmpPresentAbsent();
+            Program prg = new Program();
+            prg.CheckEmpPresentAbsent();
         }
     }
 }
