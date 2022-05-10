25c1307b5577e6642ed23909f13d4216c4b7a62c
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-05-10 16:17:55 +0530

    [Biplab] Add : Employee wages Calculation

--- Random_EmployeeCheck/Program.cs
+++ Random_EmployeeCheck/Program.cs
@@ -1,12 +1,37 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Random_EmployeeCheck
+namespace EmployeePresentAbsent
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        public int EmpPresent = 1;
+        public int FullDayHour = 8;
+        public int WagesPerHour = 20;
+        public void CheckEmpPresentAbsent()
         {
-            Console.WriteLine("Hello World!");
+            Random check = new Random();
+            int CheckEmp = check.Next(0, 2);
+
+            if (EmpPresent == CheckEmp)
+            {
+                Console.WriteLine("Employee is Present");
+                int DailyWages = FullDayHour * WagesPerHour;
+                Console.WriteLine("DailyWages: " + DailyWages);
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                Console.WriteLine("Employee has no wages");
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Program Obj = new Program();
+            Obj.CheckEmpPresentAbsent();
         }
     }
 }
