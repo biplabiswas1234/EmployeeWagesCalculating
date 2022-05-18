6d0c93eaab4d6581e11d1fbfc94ae1dd3914dd3f
biplabiswas1234 <biplabiswas1234@gmail.com>
2022-05-18 20:06:44 +0530

    UC7[Biplab] Add: Employee Wages Using Class Method

---
+++ Random_EmployeeCheck/MaxEmpHou100Days20.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EmployeePresentAbsent
+{
+    public class MaxEmpHou100Days20
+    {
+        public int EmpHour = 0;
+        public int WagesPerHour = 20;
+        public const int IsFullTime = 1;
+        public const int IsPartTime = 2;
+        public int EmployeeDay = 1;
+        public int TotalWages = 0;
+        public int TotalHours = 0;
+        public void EmployeeWages()
+        {
+            while (EmployeeDay <= 20 && TotalHours <= 100)
+              {
+                Random EmpType = new Random();
+                int EmpCheck = EmpType.Next(0, 3);
+                switch (EmpCheck)
+                {
+                    case IsFullTime:
+                        EmpHour = 8;
+                        Console.WriteLine("Employee is full time");
+                        break;
+                    case IsPartTime:
+                        EmpHour = 4;
+                        Console.WriteLine("Employee is part time");
+                        break;
+                    default:
+                        EmpHour = 0;
+                        Console.WriteLine("Employee is absent");
+
+                        break;
+                }
+                int EmpWages = EmpHour * WagesPerHour;
+                TotalWages += EmpWages;
+                TotalHours += EmpHour;
+                EmployeeDay++;
+
+            }
+            Console.WriteLine("Total Wages:" + TotalWages);
+            Console.WriteLine("Total days:" + (EmployeeDay-1));
+
+
+        }
+
+
+    }
+}
+
+        
--- Random_EmployeeCheck/Program.cs
+++ Random_EmployeeCheck/Program.cs
@@ -1,48 +1,13 @@
 ﻿using System;
 
-namespace WagesCalculation
+namespace EmployeePresentAbsent
 {
     internal class Program
     {
-        public int EmpHour = 0;
-        public int WagesPerHour = 20;
-        public const int IsFullTime = 1;
-        public const int IsPartTime = 2;
-        public int EmployeeDay = 1;
-        public int TotalWages = 0;
-        public int TotalHours = 0;
-        public int EmployeeWages()
-        {
-            while (EmployeeDay <= 20 || TotalHours <= 100)
-            {
-                Random EmpType = new Random();
-                int EmpCheck = EmpType.Next(0, 3);
-                switch (EmpCheck)
-                {
-                    case IsFullTime:
-                        EmpHour = 8;
-                        break;
-                    case IsPartTime:
-                        EmpHour = 4;
-                        break;
-                    default:
-                        EmpHour = 0;
-                        break;
-                }
-                
-                    int EmpWages = EmpHour * WagesPerHour;
-                    TotalWages += EmpWages;
-                    TotalHours += EmpHour;
-                    EmployeeDay++;
-                }
-            }
-            return TotalWages;
-        }
-
         static void Main(string[] args)
         {
-            Program wages = new Program();
-            Console.WriteLine("wages of the month:" + wages.EmployeeWages());
+            MaxEmpHou100Days20 checkEmpwages = new MaxEmpHou100Days20();
+            checkEmpwages.EmployeeWages();
         }
     }
 }
