614002774d3fc3b40edde50e646da77f8344abf7
Biplab Biswas <103117875+biplabiswas1234@users.noreply.github.com>
2022-05-31 08:33:28 +0530

    Update Program.cs

--- Random_EmployeeCheck/Program.cs
+++ Random_EmployeeCheck/Program.cs
@@ -6,7 +6,7 @@ namespace Random_EmployeeCheck
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Welcome to Employee wages calculation");
         }
     }
 }
