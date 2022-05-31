dd1c1c083528a3d7efe104eb9bf39e60913d6e42
Biplab Biswas <103117875+biplabiswas1234@users.noreply.github.com>
2022-05-31 08:35:53 +0530

    Update Program.cs

--- Random_EmployeeCheck/Program.cs
+++ Random_EmployeeCheck/Program.cs
@@ -1,12 +1,34 @@
-﻿using System;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
 
-namespace Random_EmployeeCheck
+namespace EmployeePresentAbsent
 {
-    internal class Program
+    public class Program
     {
-        static void Main(string[] args)
+        
+        public void CheckEmpPresentAbsent()
         {
-            Console.WriteLine("Welcome to Employee wages calculation");
+            Random check = new Random();
+            int CheckEmp = check.Next(0, 2);
+
+            if (EmpPresent == CheckEmp)
+            {
+                Console.WriteLine("Employee is Present");
+                
+            }
+            else
+            {
+                Console.WriteLine("Employee is Absent");
+                Console.WriteLine("Employee has no wages");
+            }
+        }
+        public static void Main(string[] args)
+        {
+            Program Obj = new Program();
+            Obj.CheckEmpPresentAbsent();
         }
     }
 }
